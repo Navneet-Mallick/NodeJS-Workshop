68981d10041977dda5d1cdb56d08e18d07bec3fc
Navneet-Mallick <navneetmallick092@gmail.com>
2025-12-18 16:31:19 +0545

    2nd

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-require("express")
+const express=require("express") 
+const app=express()
+
+app.get("/",function(haha,hehe){
+    hehe.send("Hello World") 
+    // anything can be named in place of request and response but sequence should be kept in mind 
+})
+
+app.get("/about",function(haha,hehe){
+    hehe.send("About World ..World is Huge ") 
+    // anything can be named in place of request and response but sequence should be kept in mind 
+})
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server has started at port 3000")  
+    
+})                                                                          
